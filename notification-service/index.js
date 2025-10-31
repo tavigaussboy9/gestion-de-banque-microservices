dd7f6f74be0fe18a359bf3a0c0f9db5c76225a62
tavigaussboy9 <prieuxolowo@hotmail.com>
2025-10-31 13:05:41 +0400

    feat: initial commit for notification-service/index.js

---
+++ notification-service/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb://mongo:27017/notificationdb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(PORT, () => {
+  console.log(`Notification service running on port ${PORT}`);
+});
